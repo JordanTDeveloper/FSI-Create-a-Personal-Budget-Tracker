7add7bb4a1ccf4c050fa7f899f21974f08e5f7e3
JordanTDeveloper <https://github.com/JordanTDeveloper>
2022-02-01 19:55:33 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for(let i=0; i<weeklyExpenseQuestions.length;i++){
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+    let numberAnswer=parseFloat(stringAnswer)
+    weeklyExpenses=weeklyExpenses + numberAnswer;
+}
+for(let i=0; i<monthlyExpenseQuestions.length;i++){
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    let numberAnswer=parseFloat(stringAnswer)
+    monthlyExpenses=monthlyExpenses + numberAnswer;
+}
+for(let i=0; i<annualExpenseQuestions.length;i++){
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    let numberAnswer=parseFloat(stringAnswer)
+    annualExpenseQuestions=monthlyExpenses + numberAnswer;
+}
+
+
+/* Bonus Question */
+// for(let i=annualExpenseQuestions.length; i>0;i--){
+//     let stringAnswer = window.prompt(annualExpenseQuestions[i-1])
+//     let numberAnswer=parseFloat(stringAnswer)
+//     annualExpenses=monthlyExpenses + numberAnswer;
+// }
+
+
+
+
